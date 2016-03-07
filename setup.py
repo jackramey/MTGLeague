50f9e602abb57be4dcf753b52c64adcfd4523fd6
Jack Ramey <jack.ramey@gmail.com>
2016-03-07 13:29:20 -0800

    Added ability to initialize admin default in mtgleague.cfg

---
+++ setup.py
@@ -0,0 +1,18 @@
+from mtgleague import app
+from mtgleague.util import db
+from mtgleague.models import User
+
+name = app.config['DEFAULT_ADMIN']
+email = app.config['DEFAULT_ADMIN_EMAIL']
+password = app.config['DEFAULT_ADMIN_PASSWORD']
+
+admin = User.query.filter_by(email=email)
+if admin is not None:
+    if not admin.admin:
+        admin.admin = True
+        db.session.commit()
+else:
+    admin = User(name, email, password)
+    db.session.add(admin)
+    db.session.commit()
+
