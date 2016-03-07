a123e818f7a63023db12d829f255b097da490d3d
Jack Ramey <jack.ramey@gmail.com>
2016-03-07 11:11:52 -0800

    Workin on getting Heroku deployment

--- config.py
+++ config.py
@@ -3,6 +3,10 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 #SQLAlchemy
-SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/mtgleague.db'
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
